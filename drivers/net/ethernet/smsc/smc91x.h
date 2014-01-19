3eb9314f5de3434e42b77d1b46d2f443d8d83c28
Geert Uytterhoeven <geert@linux-m68k.org>
2014-01-19 10:52:43 +0100

    net: smc91x: Remove accidentally introduced casts
    arm/lpd270_defconfig:
    
        drivers/net/ethernet/smsc/smc91x.c: In function 'smc_rcv':
        drivers/net/ethernet/smsc/smc91x.c:490:3: warning: passing argument 5 of 'smc_pxa_dma_insw' from incompatible pointer type [enabled by default]
        drivers/net/ethernet/smsc/smc91x.h:423:1: note: expected 'u_char *' but argument is of type 'u16 *'
    
    Introduced by commit afb3df11f1c8925811a1087f2351b0738a3146a2 ("m68k: Atari
    EtherNAT warning fixes").
    
    Remove two casts to fix this:
      - The first cast was in the !CONFIG_ATARI case, so it didn't matter for
        us anyway,
      - The second cast is for a pointer parameter we don't do arithmetic
        operations on, so it's also not needed.
    
    Reported-by: kbuild test robot <fengguang.wu@intel.com>
    Signed-off-by: Geert Uytterhoeven <geert@linux-m68k.org>

--- drivers/net/ethernet/smsc/smc91x.h
+++ drivers/net/ethernet/smsc/smc91x.h
@@ -1179,7 +1179,7 @@ static const char * chip_ids[ 16 ] =  {
 				SMC_outsw(ioaddr, DATA_REG(lp), __ptr, 1); \
 			}						\
 		} else if (SMC_16BIT(lp))				\
-			SMC_outsw(ioaddr, DATA_REG(lp), (u16 *) p, (l) >> 1);	\
+			SMC_outsw(ioaddr, DATA_REG(lp), p, (l) >> 1);	\
 		else if (SMC_8BIT(lp))				\
 			SMC_outsb(ioaddr, DATA_REG(lp), p, l);	\
 	} while (0)
@@ -1215,7 +1215,7 @@ static const char * chip_ids[ 16 ] =  {
 			__len += 2;					\
 			SMC_insl(__ioaddr, DATA_REG(lp), __ptr, __len>>2); \
 		} else if (SMC_16BIT(lp))				\
-			SMC_insw(ioaddr, DATA_REG(lp), (u16 *) p, (l) >> 1);	\
+			SMC_insw(ioaddr, DATA_REG(lp), p, (l) >> 1);	\
 		else if (SMC_8BIT(lp))				\
 			SMC_insb(ioaddr, DATA_REG(lp), p, l);		\
 	} while (0)
